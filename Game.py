14a7a61cf967d81419c9ed1ae6f5bb0d6f7fa7ab
TruffleTheProto <angel.perez109@upr.edu>
2025-09-10 23:45:07 -0400

    BONUS: Main Menu and Settings Added. Not much but it's honest work.

--- Game.py
+++ Game.py
@@ -9,30 +9,30 @@ speed = 10
 
 def background_music():
     # Depending on the menu you're in, just music is on.
-    global song, music_check
+    global song, music_check, volume
 
     if song == 1:
         mixer.music.load("resources/sounds/anomalocaris.mp3")
-        mixer.music.set_volume(0.4)
+        mixer.music.set_volume(volume)
         pygame.mixer.music.play(-1)
 
     # Main Menu Theme
     elif song == 2:
         mixer.music.load("resources/sounds/weevil.mp3")
-        mixer.music.set_volume(0.4)
+        mixer.music.set_volume(volume)
         pygame.mixer.music.play(-1)
 
         # Loser's Theme
     else:
         mixer.music.load("resources/sounds/snail.mp3")
-        mixer.music.set_volume(0.3)
+        mixer.music.set_volume(volume)
         pygame.mixer.music.play(-1)
 
 def ball_movement():
     """
     Handles the movement of the ball and collision detection with the player and screen boundaries.
     """
-    global ball_speed_x, ball_speed_y, score, start, game_level, speed, current_color
+    global ball_speed_x, ball_speed_y, score, start, game_level, speed, current_color, song, newgame
 
     # Base Ball Movement
     ball.x += ball_speed_x
@@ -102,22 +102,38 @@ def ball_movement():
 
     # Ball goes below the bottom boundary (missed by player)
     if ball.bottom > screen_height:
-        restart()  # Reset the game
+        restart() # Reset the game
+
+def restart():
+    """
+    Resets the ball and player scores to the initial state.
+    """
+    global ball_speed_x, ball_speed_y, score, newgame, speed, song
+    ball.center = (int(screen_width / 2), int(screen_height / 2))  # Reset ball position to center
+    ball_speed_y, ball_speed_x = 0, 0  # Stop ball movement
+    speed = 5
+    score = 0  # Reset player score
+    newgame = True
+    song = 2
+    background_music()
+    mainmenu()
 
 def paddle_hit():
-    global ball_speed_x, ball_speed_y, score, start, game_level, player, ball, speed
+    global ball_speed_x, ball_speed_y, score, start, game_level, player, ball, speed, ballphys
 
-    distance = 2 * (ball.centerx - player.centerx) / player.width
-    ball_speed_x = distance * speed
+    if not ballphys:
+        distance = 2 * (ball.centerx - player.centerx) / player.width
+        ball_speed_x = distance * speed
 
     # ==== More realistic vector based engine, kinda boring to play though ====
-    # distance = ball.center[0] - player.center[0], ball.center[1] - player.center[1]
-    # magnitude = math.sqrt(distance[0]**2 + distance[1]**2)
+    elif ballphys:
+        distance = ball.center[0] - player.center[0], ball.center[1] - player.center[1]
+        magnitude = math.sqrt(distance[0]**2 + distance[1]**2)
 
-    # direction = distance[0] / magnitude, distance[1] / magnitude
-    # ball_speed = speed * direction[0], speed * direction[1]
-    # ball_speed_y = ball_speed[1]
-    # ball_speed_x = ball_speed[0]
+        direction = distance[0] / magnitude, distance[1] / magnitude
+        ball_speed = speed * direction[0], speed * direction[1]
+        ball_speed_y = ball_speed[1]
+        ball_speed_x = ball_speed[0]
 
 def player_movement():
     """
@@ -130,17 +146,6 @@ def player_movement():
         player.left = 0
     if player.right >= screen_width:
         player.right = screen_width
-
-def restart():
-    """
-    Resets the ball and player scores to the initial state.
-    """
-    global ball_speed_x, ball_speed_y, score, newgame, speed
-    ball.center = (int(screen_width / 2), int(screen_height / 2))  # Reset ball position to center
-    ball_speed_y, ball_speed_x = 0, 0  # Stop ball movement
-    speed = 5
-    score = 0  # Reset player score
-    newgame = True
 
 # ==== Main game loop ====
 def gameplay():
@@ -202,29 +207,17 @@ def gameplay():
 
 # ======= WIP MAIN MENU =======
 
-def clamp(n, min_value, max_value):
-    return max(min_value, min(n, max_value))
-
-def shop():
-    print("temp")
-
-def cosmetics():
-    print("temp")
-
 def settings():
-    print("temp")
-
-def mainmenu():
-    global newgame, start, music_check, song, screen
+    global newgame, start, music_check, song, screen, choice2, volume, ballphys
     music_check = False  # Reset music flag
-
     while True:
         screen = pygame.display.get_surface()
-        screen.fill("black")
+        screen.blit(menu_bg, (0, 0))
 
-        option = [["start", "shop", "cosmetics", "quit"], ["main", "settings"]]
-        choice1 = clamp(1, 1, 4)
-        choice2 = clamp(1, 1, 2)
+        if choice2 < 1:
+            choice2 = 1
+        elif choice2 > 2:
+            choice2 = 2
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # Quit the game
@@ -232,50 +225,100 @@ def mainmenu():
                 sys.exit()
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and choice1 >= 1:
-                    choice1 -= 1 # Move up an option
-                if event.key == pygame.K_DOWN and choice1 <= 3:
-                    choice1 += 1 # Move down an option
-                if event.key == pygame.K_LEFT and choice2 >= 1:
-                    choice2 -= 1 # Move left
-                if event.key == pygame.K_RIGHT and choice2 <= 2:
-                    choice2 += 1 # Move right
+                if event.key == pygame.K_UP:
+                    choice2 -= 1  # Move up an option
+                elif event.key == pygame.K_DOWN:
+                    choice2 += 1  # Move down an option
 
                 # Press Button
-                if event.key == pygame.K_RETURN and newgame and option[0] == "start" and option[1] == "main":
-                    song = 1
-                    background_music()
-                    gameplay()
-                elif event.key == pygame.K_RETURN and newgame and option[0] == "shop" and option[1] == "main":
-                    song = 2
-                    shop()
-                elif event.key == pygame.K_RETURN and newgame and option[0] == "cosmetics" and option[1] == "main":
-                    song = 2
-                    cosmetics()
-                elif event.key == pygame.K_RETURN and newgame and option[0] == "quit" and option[1] == "main":
-                    pygame.quit()
-                    sys.exit()
-                elif event.key == pygame.K_RETURN and newgame and option[1] == "settings":
-                    song2 = 2
-                    settings()
+                if event.key == pygame.K_RETURN and choice2 == 1:
+                    mainmenu()
+                elif event.key == pygame.K_RETURN and choice2 == 2 and not ballphys:
+                    ballphys = True
+                elif event.key == pygame.K_RETURN and choice2 == 2 and ballphys:
+                    ballphys = False
 
+        settings2_rect = settings2_surface.get_rect(center=settings_box.center)
+        settings3_rect = settings3_surface.get_rect(center=settings3_box.center)
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    choice1 += 1  # Stop moving up
-                if event.key == pygame.K_DOWN:
-                    choice1 -= 1  # Stop moving down
-                if event.key == pygame.K_LEFT:
-                    choice2 += 1 # Stop moving left
-                if event.key == pygame.K_RIGHT:
-                    choice2 -= 1 # Stop moving right
+        if choice2 == 1:
+            pygame.draw.rect(screen, "white", settings_outline)
+        elif choice2 == 2:
+            pygame.draw.rect(screen, "white", settings3_outline)
+
+        # Square
+        pygame.draw.rect(screen, box_color, settings_box)
+        screen.blit(settings2_surface, settings2_rect)
+
+        pygame.draw.rect(screen, box_color, settings3_box)
+        screen.blit(settings3_surface, settings3_rect)
 
         # Update display
         pygame.display.flip()
         clock.tick(60)
 
-def losing_screen():
-    print("temp")
+def mainmenu():
+    global newgame, start, music_check, song, screen, choice1, speed, score
+    music_check = False  # Reset music flag
+
+    while True:
+        screen = pygame.display.get_surface()
+        screen.blit(menu_bg, (0, 0))
+
+        if choice1 < 1:
+            choice1 = 1
+        elif choice1 > 3:
+            choice1 = 3
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # Quit the game
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    choice1 -= 1 # Move up an option
+                elif event.key == pygame.K_DOWN:
+                    choice1 += 1 # Move down an option
+
+                # Press Button
+                if event.key == pygame.K_RETURN and choice1 == 1:
+                    song = 1
+                    background_music()
+                    gameplay()
+                elif event.key == pygame.K_RETURN and choice1 == 2:
+                    song = 2
+                    settings()
+                elif event.key == pygame.K_RETURN and choice1 == 3:
+                    pygame.quit()
+                    sys.exit()
+
+
+        # Main Menu Outlines
+        if choice1 == 1:
+            pygame.draw.rect(screen, "white", start_outline)
+        elif choice1 == 2:
+            pygame.draw.rect(screen, "white", settings_outline)
+        elif choice1 == 3:
+            pygame.draw.rect(screen, "white", quit_outline)
+
+        # Main Menu Blank Squares
+        pygame.draw.rect(screen, box_color, start_box)
+        pygame.draw.rect(screen, box_color, settings_box)
+        pygame.draw.rect(screen, box_color, quit_box)
+
+        # Main Menu Text
+        start_rect = start_surface.get_rect(center=start_box.center)
+        settings_rect = settings_surface.get_rect(center=settings_box.center)
+        quit_rect = quit_surface.get_rect(center=quit_box.center)
+
+        screen.blit(start_surface, start_rect)
+        screen.blit(settings_surface, settings_rect)
+        screen.blit(quit_surface, quit_rect)
+
+        # Update display
+        pygame.display.flip()
+        clock.tick(60)
 
 # General setup
 pygame.mixer.pre_init(44100, -16, 2, 1024)
@@ -295,6 +338,8 @@ screen = pygame.display.set_mode((width, height), flags)
 pygame.display.set_caption('Pong')  # Set window title
 
 # Colors
+menu_bg = pygame.image.load("resources/textures/forest.png")
+box_color = pygame.Color("black")
 bg_color = pygame.Color('grey12')
 
 # Game Rectangles
@@ -303,6 +348,21 @@ ball = pygame.Rect(screen_width / 2 - 10, screen_height / 2 - 15, 20, 20)  # Bal
 player_height = 15
 player_width = 200
 player = pygame.Rect(screen_width/2 - 45, screen_height - 30, player_width, player_height)  # Player paddle
+
+# Main Menu Rectangles
+box_width = 350
+box_height = height / 5
+outline_thickness = 4  # Thickness of the outline
+
+start_box = pygame.Rect(25, 75, box_width, box_height)
+settings_box = pygame.Rect(25, 200, box_width, box_height)
+quit_box = pygame.Rect(25, 325, box_width, box_height)
+settings3_box = pygame.Rect(25, 325, box_width, box_height)
+
+start_outline = pygame.Rect(start_box.x - outline_thickness, start_box.y - outline_thickness, start_box.width + outline_thickness * 2, start_box.height + outline_thickness * 2)
+settings_outline = pygame.Rect(settings_box.x - outline_thickness, settings_box.y - outline_thickness, start_box.width + outline_thickness * 2, start_box.height + outline_thickness * 2)
+quit_outline = pygame.Rect(quit_box.x - outline_thickness, quit_box.y - outline_thickness, start_box.width + outline_thickness * 2, start_box.height + outline_thickness * 2)
+settings3_outline = pygame.Rect(settings3_box.x - outline_thickness, settings3_box.y - outline_thickness, start_box.width + outline_thickness * 2, start_box.height + outline_thickness * 2)
 
 # Paddle colors
 paddle_colors= [pygame.Color('red'), pygame.Color('green'), pygame.Color('yellow'), pygame.Color('blue')]
@@ -313,14 +373,28 @@ ball_speed_y = 0.0
 player_speed = 0
 game_level = -1 # Resetting value to speed up the ball. Might be redundant.
 newgame = True # Indicates whether a new game can be begun.
+choice1 = 1
+choice2 = 1
+volume = 0.4
+ballphys = False
 
-song = 1
+song = 2
 music_check = False
 
 # Score Text setup
 score = 0
 high_score = 0
-basic_font = pygame.font.Font('freesansbold.ttf', 32)  # Font for displaying score
+basic_font = pygame.font.Font('freesansbold.ttf', 32)  # Font
+small_font = pygame.font.Font('freesansbold.ttf', 12)  # Font
+
+
+start_surface = basic_font.render("Start", True, (255, 255, 255))
+settings_surface = basic_font.render("Settings", True, (255, 255, 255))
+quit_surface = basic_font.render("Quit", True, (255, 255, 255))
+
+settings2_surface = small_font.render(f"NO SETTINGS!!!! Press Enter To Exit", True, (255, 255, 255))
+settings3_surface = small_font.render(f"jk, click on this for realistic ball physics", True, (255, 255, 255))
+
 
 #Milestones popups
 milestones_popups= []
@@ -329,4 +403,5 @@ milestones_messages= ["Good job!", "Amazing!", "Nice!", "Keep it up!", "Faster!"
 
 start = False  # Indicates if the game has started
 
-gameplay()
+background_music()
+mainmenu()
